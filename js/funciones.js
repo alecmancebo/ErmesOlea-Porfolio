7bad56bb08b83cb8c7a0f8bfc532e3e5d4558012
Alec <alek.rmancebo@gmail.com>
2026-01-02 12:03:31 +0100

    primer commit

---
+++ js/funciones.js
@@ -0,0 +1,169 @@
+
+//desplegar menú en movil
+const nav = document.querySelector(".menu");
+const botones = document.querySelectorAll(".boton--abrir, .menu__boton--cerrar");
+
+
+    botones.forEach(boton => {
+        boton.addEventListener("click", () => {
+            nav.classList.toggle("menu--desplegado");
+            })
+    });
+
+//hover de enlaces de menu y animación letrtas landing page
+const menuEnlaces = document.querySelectorAll(".menu__item a");
+
+const punkImgsArchivo = [
+ 'imagenes/punk_letters/archivo_letras_01.svg',
+    'imagenes/punk_letters/archivo_letras_02.svg',
+    'imagenes/punk_letters/archivo_letras_03.svg'];
+
+const punkImgsSobreMi = [
+    'imagenes/punk_letters/sobremi_letras_01.svg',
+    'imagenes/punk_letters/sobremi_letras_02.svg',
+    'imagenes/punk_letters/sobremi_letras_03.svg'
+];
+
+const punkImgsContacto = [
+    'imagenes/punk_letters/contacto_letras_01.svg',
+    'imagenes/punk_letters/contacto_letras_02.svg',
+    'imagenes/punk_letters/contacto_letras_03.svg'
+];
+
+const punkImagesSandrune = [
+    'imagenes/landing/lg_sandrune_01.svg',
+    'imagenes/landing/lg_sandrune_02.svg',
+];
+
+
+function animarLetras(img, imagenes) {
+
+  if (!img || !imagenes || imagenes.length === 0) return null;
+
+  let i = 0;
+  const intervalo = setInterval(() => {
+
+    if (!img || typeof img.src === 'undefined') {
+      clearInterval(intervalo);
+      return;
+    }
+
+    img.src = imagenes[i];
+    i = (i + 1) % imagenes.length;
+  }, 100);
+
+  return intervalo;
+
+}
+
+function hoverMenu(enlace, imagenes) {
+    let intervalo;
+    const img = enlace.querySelector("img");
+    const span = enlace.querySelector("span");
+
+    enlace.addEventListener("mouseenter", () => {
+        img.classList.add("menu__img--hover");
+        span.classList.add("menu__span--hover");
+        intervalo = animarLetras(img, imagenes);
+    });
+
+    enlace.addEventListener("mouseleave", () => {
+        img.classList.remove("menu__img--hover");
+        span.classList.remove("menu__span--hover");
+        clearInterval(intervalo);
+    });
+}
+
+hoverMenu(document.querySelector('.menu__link--archivo'), punkImgsArchivo);
+hoverMenu(document.querySelector('.menu__link--sobremi'), punkImgsSobreMi);
+hoverMenu(document.querySelector('.menu__link--contacto'), punkImgsContacto);
+
+animarLetras(document.querySelector('.letras__img'), punkImagesSandrune);
+
+//rotacion letras
+const parrafos = document.querySelectorAll(".archivo__celda.archivo__celda--proyecto");
+
+    parrafos.forEach(parrafo => {
+    parrafo.innerHTML = parrafo.innerText.split('').map(letra => letra === ' ' ? ' ' : `<span class="archivo__celda--letra">${letra}</span>`).join('');
+
+    const spans = parrafo.querySelectorAll('.archivo__celda--letra');
+    spans.forEach(span => {
+    span.style.display = 'inline-block';
+
+    const rot = Math.floor(Math.random() * 21) - 10;
+    const cambioTamaño = Math.random() * 0.2;
+    const remactual = 0.4;
+
+    span.style.transform = `rotate(${rot}deg)`;
+    span.style.fontSize = `${remactual + cambioTamaño}rem`;
+    });
+    });
+
+// ordenar/desordenar
+const botonOrdenar = document.querySelector(".ventana__boton--ordenar");
+const botonDesordenar = document.querySelector(".ventana__boton--desordenar");
+const ventanaContenedor = document.querySelector(".ventana");
+const itemsVentana = document.querySelectorAll(".ventana__item--drag, .cinta");
+
+if (botonOrdenar && botonDesordenar) {
+    
+    botonOrdenar.addEventListener("click", () => {
+        ventanaContenedor.classList.add("ordenada"); 
+        
+        itemsVentana.forEach(item => {
+            item.style.transform = "none";
+        });
+    });
+
+    botonDesordenar.addEventListener("click", () => {
+        ventanaContenedor.classList.remove("ordenada");
+        
+        itemsVentana.forEach(item => {
+            item.style.transform = "";
+        });
+    });
+}
+
+
+// click en el sobre de contacto.html
+const sobre = document.querySelector(".sobre");
+const cinta = document.querySelector(".sobre__cinta")
+
+if (cinta && sobre) {
+cinta.addEventListener('click', () => {
+    sobre.classList.toggle('esta-abierto');
+}); }
+
+
+//función click secciones en sobremi.html
+    const secciones = [
+        { el: document.querySelector(".textosm"), nivel: 3 },
+        { el: document.querySelector(".skills"), nivel: 2 },
+        { el: document.querySelector(".experiencia"), nivel: 1 }
+    ];
+
+    secciones.forEach((seccion) => {
+        if (!seccion.el) return;
+
+        seccion.el.addEventListener("click", () => {
+            const EstaEnfocada = seccion.el.classList.contains("seccion--enfocada");
+
+            if (EstaEnfocada) {
+
+                secciones.forEach(s => s.el.classList.remove("seccion--enfocada", "seccion--apartada"));
+            } else {
+                secciones.forEach(s => {
+
+                    if (s.nivel > seccion.nivel) {
+                        s.el.classList.add("seccion--apartada");
+                        s.el.classList.remove("seccion--enfocada");
+                    } else if (s.nivel === seccion.nivel) {
+                        s.el.classList.add("seccion--enfocada");
+                        s.el.classList.remove("seccion--apartada");
+                    } else {
+                        s.el.classList.remove("seccion--apartada", "seccion--enfocada");
+                    }
+                });
+            }
+        });
+    });
